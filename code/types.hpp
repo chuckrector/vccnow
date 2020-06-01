2cb4fab6a54bcf55fd4a1ea2129a062f5945b46b
Chuck Rector <choosetheforce@gmail.com>
2020-05-31 19:00:20 -0700

    kill stdint.h

--- code/types.hpp
+++ code/types.hpp
@@ -1,18 +1,16 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
-#include <stdint.h>
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef char s8;
+typedef short int s16;
+typedef int s32;
+typedef long long s64;
 
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
-
-typedef int64_t b64;
+typedef long long b64;
 
 #endif // TYPES_HPP
